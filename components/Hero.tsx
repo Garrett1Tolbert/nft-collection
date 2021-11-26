317fb8d10688a2f91b220119d760cf1301796286
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-11-26 17:26:15 -0600

    feat: added auth hook to connect wallet

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,17 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Flex, Heading, Text } from '@chakra-ui/react';
 
 export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, #7928CA, #FF0080)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-)
-
-Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
-}
+	<Flex
+		justifyContent="center"
+		alignItems="center"
+		height="100vh"
+		flexDir="column"
+		bgGradient="linear(to-l, #7928CA, #FF0080)"
+		bgClip="text"
+	>
+		<Heading fontSize="10vw">{title}</Heading>
+		<Text fontWeight="medium" color="white">
+			Uniquely yours. Each beautiful. Discover your NFT today ⚡️
+		</Text>
+	</Flex>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,34 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Box } from '@chakra-ui/react';
+import { Hero } from '../components/Hero';
+import { Container } from '../components/Container';
+import { Main } from '../components/Main';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import useAuth from '../hooks/useAuth';
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+const Index = () => {
+	const { account, connectWallet } = useAuth();
+	const label = account ? 'Mint your NFT' : 'Connect Wallet';
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+	return (
+		<Container height="100vh">
+			<Hero title="NFTsAreUs" />
+			<Main>
+				<Box
+					as="button"
+					p={4}
+					w="65%"
+					color="white"
+					fontWeight="bold"
+					borderRadius="md"
+					onClick={!account ? connectWallet : null}
+					bgGradient="linear(to-r, teal.500, green.500)"
+				>
+					{label}
+				</Box>
+			</Main>
+			<DarkModeSwitch />
+		</Container>
+	);
+};
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default Index;
